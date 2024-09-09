f346adf618c5426bd4416272facf1453a5978de1
Fabian Regul <yustif30@gmail.com>
2024-09-09 00:52:54 -0500

    version 1.0

---
+++ script.js
@@ -0,0 +1,16 @@
+//Ir a atletismo
+document.getElementById('tema_uno').addEventListener('click', function() {
+    window.location.href = 'futbol.html';
+});
+//Ir a futbol
+document.getElementById('tema_dos').addEventListener('click', function() {
+    window.location.href = 'futbol.html';
+});
+//Ir a baloncesto
+document.getElementById('tema_tres').addEventListener('click', function() {
+    window.location.href = 'baloncesto.html';
+});
+//Ir a voleibol
+document.getElementById('tema_cuatro').addEventListener('click', function() {
+    window.location.href = 'voleibol.html';
+});
